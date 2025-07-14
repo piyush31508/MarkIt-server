7dbca0dc60c3b589cac6eb268fb643e54e1a6375
piyush31508 <chawlapiyush811@gmail.com>
2025-07-14 23:57:35 +0530

    origin changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://mark-it-client.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
