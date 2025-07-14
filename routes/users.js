d932d190d28bf0e3c3bb92763a949897e5680b67
piyush31508 <chawlapiyush811@gmail.com>
2025-07-14 15:49:11 +0530

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { isAuth } from './middleware/isAuth';
+import { isAuth } from './middleware/isAuth.js';
 import { registerUser, loginUser, getMe } from '../controllers/userController.js';
 
 const router = express.Router();
